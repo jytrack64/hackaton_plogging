3d02c23807067d96b14ecfb48f3c90eb815a9822
ggyu777 <12161655@inha.edu>
2022-05-26 19:58:16 +0900

    cooperation start

---
+++ src/component/EventInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EventInfo() {
+  return (
+    <div>행사정보창</div>
+  )
+}
---
+++ src/component/Festival.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Festival() {
+  return (
+    <div>Festival</div>
+  )
+}
