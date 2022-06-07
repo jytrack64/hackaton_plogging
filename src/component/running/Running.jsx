277e6d6d768cfe4e05494cf25f3119c4b00e0999
현병택 <hyeonbyeongtaeg@hyeonbyeongtaegs-MacBook-Air.local>
2022-06-07 15:57:37 +0900

    main

--- src/component/running/Running.jsx
+++ src/component/running/Running.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Running() {
   return (
-    <div>Running</div>
-  )
+    <>
+      <div>Running</div>
+    </>
+  );
 }
 
 export default Running;
