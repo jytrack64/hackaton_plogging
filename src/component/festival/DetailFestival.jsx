085710a9701fc29ca4bff4c7024cc098c87f8943
현병택 <hyeonbyeongtaeg@hyeonbyeongtaegs-MacBook-Air.local>
2022-06-10 17:54:05 +0900

    path and detail page fix

--- src/component/festival/DetailFestival.jsx
+++ src/component/festival/DetailFestival.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Tabs, Nav } from "react-bootstrap";
+import { Tab, Tabs, Nav, Col, Container, Row, Card } from "react-bootstrap";
 import "../../css/DetailFestival.css";
+import "../../css/Scroll.css";
+import "../../css/FestivalCard.css";
+import { useNavigate } from "react-router-dom";
 
 function DetailFestival(props) {
+  let navigate = useNavigate();
   let { id } = useParams();
   const [flag, setFlag] = useState(false);
   const [tabnum, setTabnum] = useState(0);
@@ -11,146 +15,230 @@ function DetailFestival(props) {
     setFlag(!flag);
   };
   const [key, setKey] = useState("home");
-
   const nums = (e) => {
     if (e.id == Number(id) + 1) {
       return e;
     }
   };
+  const [post, setPost] = useState([]);
+  let [fade, setFade] = useState("");
+  useEffect(() => {
+    setPost(props.data);
+  });
   let findDataNum = props.data.findIndex(nums);
-  console.log(props.data[findDataNum].image);
+
   return (
     <>
-      <div
-        className="detail_image_container"
-        style={{
-          backgroundImage: "url(" + `${props.data[findDataNum].image}` + ")",
-        }}
-      ></div>
-      <div className="detail_title_container">
-        <h3 className="detail_title">{props.data[findDataNum].title}</h3>
-        <div className="detail_button_container">
-          <button
-            className="detail_cardBtn"
-            onClick={() => {
-              handleClick();
-            }}
-          >
-            <img
-              src={flag ? "/clicklike.png" : "/like.png"}
-              style={{ width: 20, userSelect: "none", right: "0" }}
-              alt="likeBtn"
+      <Container>
+        <Row>
+          <Col md={6} lg={6}>
+            <Card.Img
+              variant="top"
+              className="detail_image_container"
+              src={`${props.data[findDataNum].image}`}
+              onError={(e) => (e.target.src = "/noimage.png")}
+              style={{
+                // width: "300px",
+                // height: "300px",
+                cursor: "pointer",
+              }}
             />
-          </button>
-          <button className="detail_shareBtn">
-            <img
-              src="/share.png"
-              style={{ width: 20, userSelect: "none", right: "0" }}
-            ></img>
-          </button>
-        </div>
-      </div>
-      <span className="detail_content">{props.data[findDataNum].content}</span>
-      <Nav onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
-        <Nav.Item>
-          <Nav.Link
-            onClick={() => {
-              setTabnum(0);
+          </Col>
+          <Col md={6} lg={6} style={{ marginTop: "30px" }}>
+            <h3 className="detail_title">{props.data[findDataNum].title}</h3>
+            <div className="detail_button_container">
+              <button
+                className="detail_cardBtn"
+                onClick={() => {
+                  handleClick();
+                }}
+              >
+                <img
+                  src={flag ? "/clicklike.png" : "/like.png"}
+                  style={{ width: 20, userSelect: "none", right: "0" }}
+                  alt="likeBtn"
+                />
+              </button>
+              <button className="detail_shareBtn">
+                <img
+                  src="/share.png"
+                  style={{ width: 20, userSelect: "none", right: "0" }}
+                ></img>
+              </button>
+            </div>
+            <div className="detail_content">
+              안녕하세요 줍고입니다
+              <br />
+              <br />
+              나와 지구를 한 걸음 더 나아가게 하는 당신의 플로깅을 응원합니다!
+              <br />
+              <br />
+              주최 측에서 제공하는 행사 정보는 아래와 같습니다
+              <br />
+              <br />
+              {props.data[findDataNum].content}
+              <br />
+              <br />
+              추가적으로 궁금하신 부분은 카카오톡으로 문의주시면 빠르게
+              답변해드리겠습니다.
+            </div>
+            <button className="detail_button_apply">신청하기</button>
+          </Col>
+          <Tabs
+            defaultActiveKey="profile"
+            id="uncontrolled-tab-example"
+            className="mb-3"
+            style={{ marginTop: "30px", fontSize: "25px" }}
+          >
+            <Tab eventKey="home" title="행사정보">
+              <div className={`start ${fade}`}>
+                <div className="detail_content_container">
+                  <span className="detail_content_line">
+                    <span className="detail_content_text">일시</span>
+                    <span className="detail_content_data">
+                      {props.data[findDataNum].date}
+                    </span>
+                  </span>
+                  <span className="detail_content_line">
+                    <span className="detail_content_text">장소</span>
+                    <span className="detail_content_data">
+                      {props.data[findDataNum].location}
+                    </span>
+                  </span>
+                  <span className="detail_content_line">
+                    <span className="detail_content_text">신청</span>
+                    <span className="detail_content_data">
+                      2022.5.20~2022.6.3
+                    </span>
+                  </span>
+                  <span className="detail_content_line">
+                    <span className="detail_content_text">문의</span>
+                    <span className="detail_content_data">서울관광공사</span>
+                  </span>
+                </div>
+              </div>
+            </Tab>
+            <Tab eventKey="profile" title="후기모음">
+              <div className={`start ${fade}`}>
+                <div className="detail_comment_container">
+                  <div className="detail_comment_content">
+                    <div className="detail_comment_profile">
+                      <img
+                        className="detail_comment_face"
+                        src="/profilePersonIcon.png"
+                      ></img>
+                      <div className="detail_comment_name">쩌니</div>
+                    </div>
+                    <div className="detail_comment_text">
+                      신청했습니다! 두근두근 너무 재밌을 것 같아요~~
+                    </div>
+                  </div>
+                  <div className="detail_comment_content">
+                    <div className="detail_comment_profile">
+                      <img
+                        className="detail_comment_face"
+                        src="/profilePersonIcon.png"
+                      ></img>
+                      <div className="detail_comment_name">쏘니</div>
+                    </div>
+                    <div className="detail_comment_text">
+                      혼자서 플로깅 행사 찾아보고 있었는데 이렇게 좋은 사이트가
+                      있다니..!! 친구들한테도 공유해야겠어요!
+                    </div>
+                  </div>
+                  <div className="detail_comment_content">
+                    <div className="detail_comment_profile">
+                      <img
+                        className="detail_comment_face"
+                        src="/profilePersonIcon.png"
+                      ></img>
+                      <div className="detail_comment_name">뀨?</div>
+                    </div>
+                    <div className="detail_comment_text">
+                      플로깅은 못참지! 신청 꾹 박고 갑니다~~
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Tab>
+          </Tabs>
+
+          <br />
+          <h5
+            style={{
+              fontSize: "30px",
+              marginTop: "30px",
+              marginBottom: "15px",
+              fontWeight: "800",
             }}
           >
-            행사정보
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            onClick={() => {
-              setTabnum(1);
-            }}
-          >
-            후기모음
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <Tab tabnum={tabnum} data={props.data} />
-      <br />
-      <h5>이런행사는 어때요?</h5>
-      <div className="detail_card_container">
-        <div
-          className="detail_card_info"
-          style={{
-            width: "100px",
-            height: "100px",
-            backgroundImage: "url(" + `${props.data[6].image}` + ")",
-          }}
-        ></div>
-        <div
-          className="detail_card_info"
-          style={{
-            width: "100px",
-            height: "100px",
-            backgroundImage: "url(" + `${props.data[7].image}` + ")",
-          }}
-        ></div>
-        <div
-          className="detail_card_info"
-          style={{
-            width: "100px",
-            height: "100px",
-            backgroundImage: "url(" + `${props.data[9].image}` + ")",
-          }}
-        ></div>
-      </div>
-      <button className="detail_button_apply">신청하기</button>
+            {props.data[findDataNum].location == "온라인"
+              ? "온라인으로 진행하는 다른 행사 추천"
+              : `${props.data[findDataNum].location}지역의 다른 행사 추천`}
+          </h5>
+          <Row className="detail_card_container">
+            {post.length
+              ? post.map((item, i) => {
+                  if (item.location == props.data[findDataNum].location) {
+                    return (
+                      <>
+                        <Col
+                          md={6}
+                          lg={3}
+                          className="card_container_true"
+                          style={{ display: "flex", flexDirection: "column" }}
+                        >
+                          <div className="card_image_container">
+                            <Card.Img
+                              variant="top"
+                              // className="cardImg"
+                              src={`${item.image}`}
+                              onError={(e) => (e.target.src = "/noimage.png")}
+                              onClick={() => {
+                                navigate(`/detail/${item.id - 1}`);
+                              }}
+                              style={{
+                                // width: "300px",
+                                height: "300px",
+                                cursor: "pointer",
+                              }}
+                            />
+                          </div>
+                          <Card.Title
+                            // className="cardTitle"
+                            onClick={() => {
+                              navigate(`/detail/${item.id - 1}`);
+                            }}
+                            style={{
+                              fontFamily: "SCDream7",
+                              cursor: "pointer",
+                              width: "100%",
+                              marginTop: "15px",
+                              fontWeight: "500",
+                            }}
+                          >
+                            {item.title}
+                          </Card.Title>
+                          <Card.Subtitle
+                            // className="cardDate"
+                            style={{
+                              fontFamily: "SCDream4",
+                              marginBottom: "70px",
+                            }}
+                          >
+                            {item.date}
+                          </Card.Subtitle>
+                        </Col>
+                      </>
+                    );
+                  }
+                })
+              : null}
+          </Row>
+        </Row>
+      </Container>
     </>
   );
 }
 
 export default DetailFestival;
-
-const Tab = (props) => {
-  let { id } = useParams();
-  let [fade, setFade] = useState("");
-  const nums = (e) => {
-    if (e.id == Number(id) + 1) {
-      return e;
-    }
-  };
-  let findDataNum = props.data.findIndex(nums);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setFade("end");
-    }, 100);
-    return () => {
-      setFade("");
-    };
-  }, [props]);
-
-  if (props.tabnum == 0) {
-    return (
-      <div className={`start ${fade}`}>
-        {" "}
-        <div className="detail_content_container">
-          <span className="detail_location">
-            일시 : {props.data[findDataNum].date}
-          </span>
-          <span className="detail_location">
-            장소 : {props.data[findDataNum].location}
-          </span>
-          <span className="detail_date">신청기간 : 2022.6.3</span>
-          <span className="detail_date">문의하기 : 서울관광공사</span>
-          <span className="detail_date">신청하기 : 홈페이지 링크</span>
-        </div>
-        <button className="detail_button_apply">신청하기</button>
-      </div>
-    );
-  } else if (props.tabnum == 1) {
-    return (
-      <div className={`start ${fade}`}>
-        <div>홍길동</div>
-        <div>홍순자</div>
-        <div>홍도리</div>
-      </div>
-    );
-  }
-};
--- src/component/running/Running.jsx
+++ src/component/running/Running.jsx
@@ -1,11 +1,10 @@
-
 import React, { useState, useCallback, useRef, useEffect } from "react";
 import { Map } from "react-kakao-maps-sdk";
 import RunningCourse from "./RunningCourse";
 import Current from "./Current";
-import '../../css/Running.css';
-import {Container,Row,Col} from 'react-bootstrap'
-import TrashMarker from './TrashMarker';
+import "../../css/Running.css";
+import { Container, Row, Col } from "react-bootstrap";
+import TrashMarker from "./TrashMarker";
 
 function Running() {
   const [flag, setFlag] = useState(true);
@@ -23,6 +22,8 @@ function Running() {
     lat: 0,
     lng: 0,
   });
+
+  const [leftButton, setLeftButton] = useState(0);
 
   // 내위치에 필요한 state
   const [current, setCurrent] = useState({
@@ -103,22 +104,22 @@ function Running() {
       <Map // 지도를 표시할 Container
         // 지도의 중심좌표
         center={current.center}
-      style={{
-        // 지도의 크기
-        width: "100vw",
-        height: "100vh",
-        zIndex: '1'
-      }}
-      level={3} // 지도의 확대 레벨
-      onClick={start == true ? handleClick : null}
-      onRightClick={start == true ? handleRightClick : null}
-      onMouseMove={start == true ? handleMouseMove : null}
+        style={{
+          // 지도의 크기
+          width: "100vw",
+          height: "100vh",
+          zIndex: "1",
+        }}
+        level={3} // 지도의 확대 레벨
+        onClick={start == true ? handleClick : null}
+        onRightClick={start == true ? handleRightClick : null}
+        onMouseMove={start == true ? handleMouseMove : null}
       >
-      <TrashMarker flag={flag}/>
-   {/* {start == true ? (
+        <TrashMarker flag={flag} />
+        {/* {start == true ? (
           <button onClick={start == true ? handleClick : null}>기록하기</button>
         ) : null} */}
-     
+
         {start == true ? (
           <RunningCourse
             moveLine={moveLine}
@@ -135,59 +136,146 @@ function Running() {
             setMousePosition={setMousePosition}
           />
         ) : null}
-
         <Current current={current} setCurrent={setCurrent} />
-       
+        {leftButton == 1 ? <LeftButton /> : null}
       </Map>
-          <Container>
+      <Container>
         <Row>
           <Col xs={12} md={12} lg={12}>
-      <div className='running'>
-        <div className='running_topBar'>
-          <div className='running_topBar_searchBar'>
-            <div className='running_topBar_searchBar_Img'></div>
-            <span className='running_topBar_searchBar_text'>ㅤ도착 위치 검색</span>
-          <div className='running_topBar_searchBar_empty'></div>
-          </div>
-          <div className='running_topBar_empty'>
-
-          </div>
-          <div className='running_topBar_trashCan' onClick={() => {setFlag(!flag)}}>
-
-          </div>
-        </div>
-        <div className='running_bottomBar_add' onClick={() =>
-            setCurrent({
-              center: {
-                lat: current.center.lat + 0.000000000001,
-                lng: current.center.lng + 0.000000000001,
-              },
-              isPanto: true,
-            })
-          }>
-
-        </div>
-        <div className='running_bottomBar'>
-          <div className='running_bottomBar_first'>
- 
-          </div>
-          <div className='running_bottomBar_second' onClick={() => {
-            setStart(true);
-          }} >
-
-          </div>
-          <div className='running_bottomBar_third'>
-            <div className='endPoint'>
-
+            <div className="running">
+              <div className="running_topBar">
+                <div className="running_topBar_searchBar">
+                  <div className="running_topBar_searchBar_Img"></div>
+                  <span className="running_topBar_searchBar_text">
+                    ㅤ도착 위치 검색
+                  </span>
+                  <div className="running_topBar_searchBar_empty"></div>
+                </div>
+                <div className="running_topBar_empty"></div>
+                <div
+                  className="running_topBar_trashCan"
+                  onClick={() => {
+                    setFlag(!flag);
+                  }}
+                ></div>
+              </div>
+              <div
+                className="running_bottomBar_add"
+                onClick={() =>
+                  setCurrent({
+                    center: {
+                      lat: current.center.lat + 0.000000000001,
+                      lng: current.center.lng + 0.000000000001,
+                    },
+                    isPanto: true,
+                  })
+                }
+              ></div>
+              <div className="running_bottomBar">
+                <div
+                  className="running_bottomBar_first"
+                  onClick={() => {
+                    setLeftButton((prev) => !prev);
+                  }}
+                ></div>
+                <div
+                  className="running_bottomBar_second"
+                  onClick={() => {
+                    setStart(1);
+                  }}
+                ></div>
+                <div className="running_bottomBar_third">
+                  <div className="endPoint"></div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
-      </Col>
-      </Row>
+          </Col>
+        </Row>
       </Container>
     </>
   );
 }
+
+const LeftButton = () => {
+  return (
+    <>
+      <div className="left_button">
+        <div className="left_button_path">
+          <div className="left_button_path_text">경로</div>
+        </div>
+        <div className="left_button_container">
+          <div className="left_button_detail">
+            <div className="left_button_detail_icon_container">
+              <div className="left_button_detail_icon">
+                <img
+                  className="left_button_detail_icon_image"
+                  src="/path.png"
+                />
+              </div>
+              <div className="left_button_detail_text">저장된 경로</div>
+            </div>
+            <div className="left_button_detail_button">
+              <img
+                className="left_button_detail_button_image"
+                src="/pathbutton.png"
+              />
+            </div>
+          </div>
+          <div className="left_button_detail">
+            <div className="left_button_detail_icon_container">
+              <div className="left_button_detail_icon">
+                <img
+                  className="left_button_detail_icon_image"
+                  src="/star.png"
+                />
+              </div>
+              <div className="left_button_detail_text">별표 표시한 구간</div>
+            </div>
+            <div className="left_button_detail_button">
+              <img
+                className="left_button_detail_button_image"
+                src="/pathbutton.png"
+              />
+            </div>
+          </div>
+          <div className="left_button_detail">
+            <div className="left_button_detail_icon_container">
+              <div className="left_button_detail_icon">
+                <img
+                  className="left_button_detail_icon_image"
+                  src="/history.png"
+                />
+              </div>
+              <div className="left_button_detail_text">코스기록</div>
+            </div>
+            <div className="left_button_detail_button">
+              <img
+                className="left_button_detail_button_image"
+                src="/pathbutton.png"
+              />
+            </div>
+          </div>
+          <div className="left_button_detail">
+            <div className="left_button_detail_icon_container">
+              <div className="left_button_detail_icon">
+                <img
+                  className="left_button_detail_icon_image"
+                  src="/trophy.png"
+                />
+              </div>
+              <div className="left_button_detail_text">지역순위</div>
+            </div>
+            <div className="left_button_detail_button">
+              <img
+                className="left_button_detail_button_image"
+                src="/pathbutton.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Running;
