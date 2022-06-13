9a521754100b1a49ade7624c45fa0160b9108264
ggyu777 <12161655@inha.edu>
2022-06-13 16:54:13 +0900

    123

---
+++ app.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function(req, res) {
+  res.sendFile(__dirname, "./build/index.html");
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
