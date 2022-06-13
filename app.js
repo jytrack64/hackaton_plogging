e145044ab237a6d4c011e0f014aa6c65a67ec5e0
ggyu777 <12161655@inha.edu>
2022-06-13 21:31:20 +0900

    Revert "123"
    This reverts commit e1344a83cd9efce872f0ae3bad12ff9e9383c4ec.

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname, "./build/index.html");
-});
-
 app.get('*', function(req, res) {
   res.sendFile(__dirname, "./build/index.html");
 });
