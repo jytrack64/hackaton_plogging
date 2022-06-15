d4eba5948718baad38f1466196142c4441f53c45
ggyu777 <12161655@inha.edu>
2022-06-15 15:27:18 +0900

    conf: nav.link href

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(__dirname, "./build/index.html");
 });
 
 app.listen(PORT, () => {
