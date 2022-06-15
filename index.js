8cd156a70c9609a545d0bf77a91b837005a97cba
ggyu777 <12161655@inha.edu>
2022-06-15 14:35:10 +0900

    refix

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express()
 const port = process.env.PORT || 5000;
 const path = require("path");
 
+import {Router} from 'express';
 
+const router = Router();
+
+router.use('/event',(req,res)=>{
+    res.send('festival page');
+})
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.urlencoded({ extended: true }));
