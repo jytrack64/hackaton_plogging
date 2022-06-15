a577134b598460324d1f247f46fd842cc090b544
ggyu777 <12161655@inha.edu>
2022-06-15 14:28:57 +0900

    refix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('./client/build'));
 }
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
