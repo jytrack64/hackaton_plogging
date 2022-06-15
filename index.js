53ff534e32dab31f4c027410f73c0c97934141f6
ggyu777 <12161655@inha.edu>
2022-06-15 14:24:16 +0900

    fix

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express()
 const port = process.env.PORT || 5000;
 const path = require("path");
 
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +19,7 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
 res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-  
+
 app.get("*", (req, res) => {
 res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
