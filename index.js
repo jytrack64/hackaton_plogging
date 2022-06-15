e5d2610d1420f9ae4fc16da3abab50953c786a34
ggyu777 <12161655@inha.edu>
2022-06-15 15:41:19 +0900

    conf: nav.link href

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -12,9 +12,9 @@ app.listen(port, () => {
 })
 
 app.get("/", (req, res) => {
-res.sendFile(path.join(__dirname, "./client/build/index.html"));
+res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
   
 app.get("*", (req, res) => {
-res.sendFile(path.join(__dirname, "./client/build/index.html"));
+res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
