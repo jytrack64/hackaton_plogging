062a7fae970e34028b99e5324d92a9e70ba7fb05
ggyu777 <12161655@inha.edu>
2022-06-15 15:17:39 +0900

    conf: nav.link href

--- index.js
+++ index.js
@@ -3,15 +3,8 @@ const app = express()
 const port = process.env.PORT || 5000;
 const path = require("path");
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
-  
-  app.get("/", (request, response) => {
-    response.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
 
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,10 +12,10 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-// app.get("/", (req, res) => {
-// res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.get("*", (req, res) => {
-    response.sendFile(path.join(__dirname, "client/build", "index.html"));
+res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
