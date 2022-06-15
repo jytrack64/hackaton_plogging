6d42c9dbb5991d0b60b5eaa2813309bfc16254cb
ggyu777 <12161655@inha.edu>
2022-06-15 14:30:35 +0900

    refix

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const port = process.env.PORT || 5000;
 const path = require("path");
 
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('./client/build'));
-}
 
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
